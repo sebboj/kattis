fc9025dfae866ab5511b0ebf23a7e4631895b5a7
Sebby <sebby331@gmail.com>
2017-02-13 21:31:06 -0500

    Delete mixedfractions.py

--- new/mixedfractions.py
+++
@@ -1,6 +0,0 @@
-a,b = input().split(" ")
-a,b = [int(a), int(b)]
-while a != 0 and b != 0:
-    print("%s %s / %s" % (str(a//b), str(a%b), str(b)))
-    a,b = input().split(" ")
-    a,b = [int(a), int(b)]
